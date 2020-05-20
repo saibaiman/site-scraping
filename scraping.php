984de28487cbf1700af967d56c0111327dd389e4
saibaiman <nagainozomi.19350511@icloud.com>
2020-05-20 23:06:45 +0900

    first commit

---
+++ scraping.php
@@ -0,0 +1,39 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Goutte\Client;
+
+try {
+	$cli = new Client();
+	$topUrl = 'https://jeek.jp/sign_in';
+	$top = $cli->request('GET', $topUrl);
+	$loginForm = $top->filter('form')->form();
+	$value = $loginForm->getValues();
+	$value['user[email]'] = 'nagainozomi.19350511@gmail.com';
+	$value['user[password]'] = 'saibaiman';
+	$method = $loginForm->getMethod();
+	$url = $loginForm->getUri();
+	$crawler = $cli->request($method, $url, $value);
+	$cookies = $cli->getCookieJar()->all();
+	sleep(2);
+
+	$cli = new Client();
+	$cli->getCookieJar()->updateFromSetCookie($cookies);
+	$crawler = $cli->request('GET', 'https://jeek.jp/mypage');
+	$text = $crawler->filter('a');
+	if ($text->count() === 0) {
+		echo 'empty';
+	}
+	$text->each(function($info) {
+		echo $info->text() ."<br>";
+	});
+	sleep(2);
+} catch(Exception $e) {
+	echo "error;" . $e->getMessage();
+}
+
+
+
+
+
+
